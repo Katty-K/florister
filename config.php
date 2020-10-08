ae9fc72e1a5acf30b3c9d695eacc86bdc291123f
Katty-K <72502415+Katty-K@users.noreply.github.com>
2020-10-08 15:16:00 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://b916236o.beget.tech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://b916236o.beget.tech/');
+
+// DIR
+define('DIR_APPLICATION', '/home/b/b916236o/b916236o.beget.tech/public_html/catalog/');
+define('DIR_SYSTEM', '/home/b/b916236o/b916236o.beget.tech/public_html/system/');
+define('DIR_IMAGE', '/home/b/b916236o/b916236o.beget.tech/public_html/image/');
+define('DIR_LANGUAGE', '/home/b/b916236o/b916236o.beget.tech/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/b/b916236o/b916236o.beget.tech/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/b/b916236o/b916236o.beget.tech/public_html/system/config/');
+define('DIR_CACHE', '/home/b/b916236o/b916236o.beget.tech/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/b/b916236o/b916236o.beget.tech/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/b/b916236o/b916236o.beget.tech/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/b/b916236o/b916236o.beget.tech/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/b/b916236o/b916236o.beget.tech/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'b916236o_fg');
+define('DB_PASSWORD', 'todestodes2012');
+define('DB_DATABASE', 'b916236o_fg');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
